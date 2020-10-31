391a18b1ebf7ba536e2aaf7407b205cb50b5ed89
Itzel Enciso <51388362+eriika19@users.noreply.github.com>
2020-10-31 01:10:18 -0600

    notes app finish
    notes app finish

--- src/components/notes-app/index.js
+++ src/components/notes-app/index.js
@@ -1,37 +1,119 @@
-import React  from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-function NotesApp () {
+function NotesApp() {
+  const [toDoTasksObj, setToDoTasksObj] = useState({
+    all: [],
+    completed: [],
+    active: [],
+  });
+  const [statusActive, setStatusActive] = useState("all");
+  const [titleValue, setTitleValue] = useState("");
+  const [statusValue, setStatusValue] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "title":
+        setTitleValue(value);
+        break;
+      case "status":
+        setStatusValue(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const getTasksArr = (activeStatus) => {
+    const status = activeStatus.toLowerCase();
+    if (status !== "all") {
+      return toDoTasksObj[status];
+    } else {
+      const tasks = [];
+      const categoriesArr = Object.keys(toDoTasksObj);
+      categoriesArr.forEach((categorie) => {
+        if (toDoTasksObj[categorie].length > 0)
+          tasks.push(...toDoTasksObj[categorie]);
+      });
+      return tasks;
+    }
+  };
+
+  const handleCategoriesChange = (categorie) => {
+    setStatusActive(categorie);
+  };
+
+  const addTask = () => {
+    const newToDoTasksObj = { ...toDoTasksObj };
+    const status = statusValue.toLowerCase();
+    const task = { title: titleValue, status };
+    if (toDoTasksObj[status]) {
+      newToDoTasksObj[status].push(task);
+    } else {
+      newToDoTasksObj[status] = [task];
+    }
+    setToDoTasksObj(newToDoTasksObj);
+    setTitleValue("");
+    setStatusValue("");
+  };
+
   return (
     <div className="layout-column align-items-center justify-content-start">
       <section className="layout-row align-items-center justify-content-center mt-30">
-        <input data-testid="input-note-name" type="text" className="large mx-8"
-              placeholder="Note Title"/>
-        <input data-testid="input-note-status" type="text" className="large mx-8"
-              placeholder="Note Status"/>
-        <button className="" data-testid="submit-button">Add Note</button>
+        <input
+          name="title"
+          data-testid="input-note-name"
+          type="text"
+          value={titleValue}
+          className="large mx-8"
+          placeholder="Note Title"
+          onChange={handleInputChange}
+        />
+        <input
+          name="status"
+          data-testid="input-note-status"
+          type="text"
+          value={statusValue}
+          className="large mx-8"
+          placeholder="Note Status"
+          onChange={handleInputChange}
+        />
+        <button className="" data-testid="submit-button" onClick={addTask}>
+          Add Note
+        </button>
       </section>
 
       <div className="mt-50">
         <ul className="tabs">
-          <li className="tab-item slide-up-fade-in" data-testid="allButton">All</li>
-          <li className="tab-item slide-up-fade-in" data-testid="activeButton">Active</li>
-          <li className="tab-item slide-up-fade-in" data-testid="completedButton">Completed</li>
+          {Object.keys(toDoTasksObj).map((categorie, index) => (
+            <li
+              key={`${categorie}-${index}`}
+              name={`${categorie}`}
+              className="tab-item slide-up-fade-in"
+              data-testid={`${categorie}Button`}
+              onClick={() => handleCategoriesChange(categorie)}
+            >
+              {`${categorie}`}
+            </li>
+          ))}
         </ul>
       </div>
       <div className="card w-40 pt-30 pb-8">
         <table>
           <thead>
-          <tr>
-            <th>Title</th>
-            <th>Status</th>
-          </tr>
+            <tr>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
           </thead>
           <tbody data-testid="noteList">
-            <tr>
-              <td></td>
-              <td></td>
-            </tr>
+            {getTasksArr(statusActive).map((task, index) => (
+              <tr key={`${task.status}-${index}`}>
+                <td>{task.title}</td>
+                <td>{task.status}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -39,4 +121,4 @@ function NotesApp () {
   );
 }
 
-export default NotesApp
+export default NotesApp;
